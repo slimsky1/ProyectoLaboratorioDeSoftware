d87fece51b8760fe8032d791ba53df1779c72358
slimsky1 <40033936+slimsky1@users.noreply.github.com>
2020-10-19 21:42:04 -0300

    Commit Inicial

---
+++ ProyectoLaboratorioDeSoftware/ProyectoLaboratorioDeSoftware/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoLaboratorioDeSoftware
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
